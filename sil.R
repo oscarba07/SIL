698cfacf05a9cf6409150b821de40ff7412ffd9d
oscarba07 <oscarba07@gmail.com>
2023-05-22 21:02:57 -0600

    Descarga de objetos de iniciativas; Limpieza de Temas.

--- sil.R
+++ sil.R
@@ -1,5 +1,5 @@
 # Carga de paquetería necesaria
-packs <- c('xml2','rvest','stringr')
+packs <- c('xml2','rvest','stringr','tidyr','dplyr','purrr')
 
 for (p in packs) {
   if (!require(p, character.only = T)) {
@@ -12,8 +12,9 @@ for (p in packs) {
 colnames <- c('No.','tipo.de.asunto','denom','clasif','origen',
               'fecha.presentacion','presentada.por','p.politico','legislatura',
               'turnado.a','estatus','tema')
-download.file(url, destfile = "scrapedpage.html",method='libcurl', quiet=TRUE)
-response <- read_html('SIL.html')
+response <- read_html(url)
+#download.file(url, destfile = "scrapedpage.html",method='libcurl', quiet=TRUE)
+#response <- read_html('SIL.html')
 tbls <- html_nodes(response, 'table')
 inic <- tbls[[6]] 
 
@@ -23,15 +24,31 @@ links <- links[grep('ContenidoAsuntos',links)]
 links <- str_extract(links,'/Librerias/pp_ContenidoAsuntos\\.php\\?SID=&Clave=\\d*')
 links <- paste0('http://sil.gobernacion.gob.mx', links)
 
-inic_obj <- function(v){
-  #download.file(v, destfile = "inic_obj.html",method='libcurl', quiet=TRUE)
-  h <- read_html("inic_obj.html")
-  h <- read_html(v)
-  h <- html_nodes(h, 'table')[[3]] %>% html_nodes(xpath='/html/body/table[3]/tbody/tr[4]/td/text()')
-}
-
 # Crear tabla
 inic <- inic %>% html_table(fill = TRUE)
 inic <- inic[-c(1:5),]
 names(inic) <-  colnames
 
+# Crear funciion para extraer objeto de iniciativas
+inic_obj <- function(v){
+  #download.file(v, destfile = "inic_obj.html",method='libcurl', quiet=TRUE)
+  #h <- read_html("inic_obj.html")
+  h <- read_html(v)
+  h <- html_nodes(h, 'table')[[3]] %>% html_table()
+  obj <- h$X1[[4]]
+  return(obj)
+}
+
+# Asignacion de objeto de iniciativa
+obj <- sapply(links,FUN = inic_obj) %>% unname()
+inic$objeto <- obj
+
+head(inic)
+
+# Split temas
+t <- str_split(inic$tema,'\\d.-')
+# Subset para eliminar blanks
+t <- lapply(t, FUN= function(x) subset(x,x!=''))
+
+inic$tema.n <- t
+inic <- inic %>% mutate(n = map_dbl(.x=tema.n,.f=length))
