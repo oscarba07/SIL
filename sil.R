3e6fbf7ceaea0d7d9c100dc98875971f3d4154b0
oscarba07 <oscarba07@gmail.com>
2023-05-25 19:33:48 -0600

    Resumen por partido politico y Grafico

--- sil.R
+++ sil.R
@@ -1,10 +1,6 @@
-url <- 'http://sil.gobernacion.gob.mx/Numeralia/Iniciativas/resultadosNumeraliaIniciativas.php?SID=&Origen=IL&Serial=334c274a03d349cf633b61b01cc8feab&Reg=466&Paginas=15&pagina=2'
-tot <- 466
-url <- sub('Paginas=\\d+', paste0('Paginas=',tot), url)
-url <- sub('pagina=2', 'pagina=1', url)
-
 # Carga de paquetería necesaria
-packs <- c('xml2','rvest','stringr','tidyr','dplyr','purrr','plyr')
+packs <- c('xml2','rvest','stringr','tidyr','dplyr','purrr','plyr','ggplot2',
+           'plotly')
 
 for (p in packs) {
   if (!require(p, character.only = T)) {
@@ -12,6 +8,13 @@ for (p in packs) {
     library(p, character.only = T)
   }
 }
+
+# Definicion de URL
+url <- 'http://sil.gobernacion.gob.mx/Numeralia/Iniciativas/resultadosNumeraliaIniciativas.php?SID=&Origen=IL&Serial=334c274a03d349cf633b61b01cc8feab&Reg=466&Paginas=15&pagina=2'
+tot <- 50
+url <- sub('Paginas=\\d+', paste0('Paginas=',tot), url)
+url <- sub('pagina=2', 'pagina=1', url)
+
 
 # Lectura de tabla
 colnames <- c('No.','tipo.de.asunto','denom','clasif','origen',
@@ -54,6 +57,7 @@ head(inic)
 t <- str_split(inic$tema,'\\d.-')
 # Subset para eliminar blanks
 t <- lapply(t, FUN= function(x) subset(x,x!='')) 
+temas <- t %>% unlist() %>% unique() %>% str_replace_all(pattern = ' ','\\.')
 # Funcion para convertir a dataframes con dummies
 todf <- function(x){
   x.names <- x
@@ -64,3 +68,23 @@ tdummy_df <- sapply(t, todf) %>% bind_rows() %>% replace(is.na(.),0)
 
 # Unir a dataframe
 inic <- cbind(inic,tdummy_df)
+
+# Limpiar casos donde la iniciativa es presentada por un partido politico
+inic[which(inic$p.politico=='-'),'p.politico'] <- inic[which(inic$p.politico=='-'),
+                                                     "presentada.por"]
+
+# Numero de iniciativas por tema y partido politico
+inic.pp <- aggregate(inic[,temas], by = list(inic$p.politico), FUN = sum)
+# Formato Base de datos
+inic.pp <- inic.pp %>% pivot_longer(cols = temas, values_to = 'n.inic')  
+# Grafico
+cols <- c('Morena'='#600c0c','PAN'='#00308f','PRD'='#ffcc01','PRI'='#006600',
+          'MC'='#f77821','PVEM'='#50b646','PT'='#da251c',
+          'Ejecutivo Federal'='#c8b64f','Congreso de Michoacán'='#691549')
+p <- ggplot(inic.pp, aes(x=n.inic,y=name, fill=Group.1)) +
+  scale_fill_manual(values=cols) +
+  geom_bar(stat="identity", colour="white") + 
+  labs(x='Numero de iniciativas',y='Tema',fill='Partido politico') +
+  scale_x_continuous(breaks=1:50)
+ggplotly(p)
+
